c1f5cced600a9344d03a304d5b584bffaf69dfa8
fsl <2865069122@qq.com>
2023-03-11 18:19:07 +0800

    push test

--- src/main/java/com/fsl/git.java
+++ src/main/java/com/fsl/git.java
@@ -7,5 +7,6 @@ public class git {
         System.out.println("hello hot-fix!");
         System.out.println("master test");
         System.out.println("hello hot-fix!");
+        System.out.println("push test!");
     }
 }
